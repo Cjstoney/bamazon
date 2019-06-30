f0062b5517b62a4f69c1aa725a535fc44831f0df
Camerone Stoney <Cameronestoney@yahoo.com>
2019-06-30 16:25:15 -0700

    list for purchase choices complete

--- app.js
+++ app.js
@@ -9,7 +9,54 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-connection.connect(function(err){
-    if(err) throw err;
-    console.log("connected")
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected");
+    customerGreeting();
 });
+
+function customerGreeting() {
+    inquirer
+        .prompt({
+            name: "customerIntro",
+            type: "list",
+            message: "Hi, are you here to make a purchase?",
+            choices: ["YES", "NO"]
+        })
+        .then(function (answer) {
+            if (answer.customerIntro === "YES") {
+                showProducts();
+            }
+            else {
+                console.log("Sorry we couldn't be of more help. Please come back again.");
+                connection.end();
+            }
+        })
+}
+
+function showProducts() {
+    connection.query("SELECT id, product_name FROM products", function (err, results) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: "product-choice",
+                    type: "rawlist",
+                    choices: function () {
+                        var prodsArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            var id = results[i].id;
+                            var name = results[i].product_name;
+                            prodsArray.push(id, name);
+                        }
+                        return prodsArray;
+                    },
+                    message: "Please select the item that you are shopping for"
+                }
+            ])
+            .then(function(answer){
+                var 
+            })
+            // ask how many they would like.
+    })
+}
